2f269cdaaccaf1fc19a998de96355d6f77f9c884
Tye Peck <tye@novatasolutions.com.au>
2022-12-08 12:23:39 +0800

    feat: add dynamic output option, add error handling

--- CSV2JSON/Program.cs
+++ CSV2JSON/Program.cs
@@ -10,26 +10,8 @@ class Program
 {
     static async Task<int> Main(string[] args)
     {
+        int returnCode = 0;
         RootCommand rootCommand = new RootCommand("Converts a CSV file to JSON");
-
-        var fileInputOption = new Option<FileInfo>(
-            aliases: new string[] { "--input", "-i" },
-            description: "The file to read and convert.",
-            parseArgument: result =>
-            {
-                string? filePath = result.Tokens.Single().Value;
-                if (!File.Exists(filePath))
-                {
-                    result.ErrorMessage = "File does not exist";
-                    return null;
-                }
-                else
-                {
-                    return new FileInfo(filePath);
-                }
-            }
-        ){ IsRequired = true };
-
 
         var fileOutputOverwriteOption = new Option<bool>(
             name: "--overwrite-existing",
@@ -39,7 +21,7 @@ class Program
 
         var fileOutputOption = new Option<FileInfo>(
             aliases: new string[] { "--output", "-o" }, 
-            description: "The file to read and convert.",
+            description: "The output file to save. Defaults to <inputFile>.json",
             parseArgument: result =>
             {
                 var isOverwrite = result.GetValueForOption(fileOutputOverwriteOption);
@@ -54,9 +36,47 @@ class Program
                     return new FileInfo(filePath);
                 }
             }
+        );
+        var fileInputOption = new Option<FileInfo>(
+            aliases: new string[] { "--input", "-i" },
+            description: "The file to read and convert.",
+            parseArgument: result =>
+            {
+
+                string? filePath = result.Tokens.Single().Value;
+                if (!File.Exists(filePath))
+                {
+                    result.ErrorMessage = "File does not exist";
+                    return null;
+                }
+                else
+                {
+                    var fileInfo = new FileInfo(filePath);
+                    var outFile = result.GetValueForOption(fileOutputOption);
+                    if(outFile == null)
+                    {
+                        var isOverwrite = result.GetValueForOption(fileOutputOverwriteOption);
+                        var autoOutFileName = Path.GetFileNameWithoutExtension(fileInfo.Name) + ".json";
+                        var autoOutFilePath = Path.Combine(fileInfo.DirectoryName, autoOutFileName);
+                        if (File.Exists(autoOutFilePath) && !isOverwrite)
+                        {
+                            result.ErrorMessage = $"Output file {autoOutFilePath} already exists. Use --overwrite-existing option to overwrite the output";
+                            return null;
+                        }
+                        else
+                        {
+                            return new FileInfo(filePath);
+                        }
+                    }
+                    else
+                    {
+                        return fileInfo;
+                    }
+                }
+            }
         )
         { IsRequired = true };
-        
+
         rootCommand.AddOption(fileInputOption);
         rootCommand.AddOption(fileOutputOption);
         rootCommand.AddOption(fileOutputOverwriteOption);
@@ -64,21 +84,39 @@ class Program
 
         rootCommand.SetHandler(async (fileIn, fileOut) =>
         {
-            await ProcessCSV(fileIn, fileOut);
+            returnCode = await ProcessCSV(fileIn, fileOut);
         }, fileInputOption, fileOutputOption);
 
-        return await rootCommand.InvokeAsync(args);
+        await rootCommand.InvokeAsync(args);
+        
+        return returnCode;
     }
-    static async Task ProcessCSV(FileInfo fileIn, FileInfo fileOut)
+    static async Task<int> ProcessCSV(FileInfo fileIn, FileInfo fileOut)
     {
-
-        using (var reader = new StreamReader(fileIn.FullName))
-        using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+        try
         {
-            var records = csv.GetRecords<dynamic>().ToList();
-            var json = JsonConvert.SerializeObject(records);
-            await File.WriteAllTextAsync(fileOut.FullName, json);
+            throw new Exception();
+            if (fileOut == null)
+            {
+                var autoOutFilePath = Path.GetFileNameWithoutExtension(fileIn.Name) + ".json";
+                fileOut = new FileInfo(Path.Combine(fileIn.DirectoryName, autoOutFilePath));
+            }
+            using (var reader = new StreamReader(fileIn.FullName))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<dynamic>().ToList();
+                var json = JsonConvert.SerializeObject(records);
+                await File.WriteAllTextAsync(fileOut.FullName, json);
+                Console.WriteLine($"Output JSON file: {fileOut.FullName}");
+                return 0;
+            }
         }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Could not convert file. {e.Message}");
+            return -2;
+        }
+        
     }
 }
 
